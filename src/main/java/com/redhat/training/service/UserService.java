9e8024040560e3189f226ec6a078cb39836378e8
Mariam Mazen <42315208+Mariammazen147@users.noreply.github.com>
2025-05-11 23:38:47 +0300

    Update UserService.java

--- src/main/java/com/redhat/training/service/UserService.java
+++ src/main/java/com/redhat/training/service/UserService.java
@@ -35,7 +35,7 @@ public class UserService {
     public User findByEmail(String email) {
         try {
             return em.createQuery(
-                "SELECT u FROM User u WHERE u.email = :email", User.class)
+                "SELECT u FROM User WHERE u.email = :email", User.class)
                 .setParameter("email", email)
                 .getSingleResult();
         } catch (Exception e) {
